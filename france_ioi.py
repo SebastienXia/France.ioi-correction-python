f4772c119f985ac3b921c7cb2b66730052e4d3c5
Sébastien Xia <sebastienxia.gmail.com>
2023-04-10 16:29:02 +0200

    first commit

---
+++ france_ioi.py
@@ -0,0 +1,106 @@
+# Enlever les triple guillemets """ [texte] """ pour que le code ne soit plus en commentaire !
+
+                                        # Niveau 1
+
+                    # 1 – Affichage de texte, suite d'instructions.
+
+# 1) Hello world!
+"""
+
+print("Hello world!")
+
+"""
+
+# 2) Présentation
+"""
+
+print("Coucou !")
+print("Je m'appelle Camthalion")
+print("Ma devise est 'Parler peu mais parler bien'.")
+
+"""
+
+# 3) Plan de la montagne
+"""
+
+print("Tout droit tu grimperas,")
+print("La clé tu trouveras,")
+print("Habile tu seras,")
+print("Quand tu les porteras,")
+print("Et avec le chef tu reviendras !")
+
+"""
+
+# 4) Dans le fourré
+"""
+
+from robot import *
+
+haut()
+haut()
+haut()
+droite()
+droite()
+bas()
+bas()
+droite()
+
+"""
+
+# 5) Empilement de cylindres
+"""
+from robot import *
+
+deplacer(1, 3)
+deplacer(1, 2)
+deplacer(3, 2)
+deplacer(1, 3)
+
+deplacer(2, 3)
+deplacer(2, 1)
+deplacer(3, 2)
+deplacer(1, 3)
+
+deplacer(2, 3)
+deplacer(1, 2)
+deplacer(3, 1)
+deplacer(3, 2)
+deplacer(1, 3)
+deplacer(2, 1)
+deplacer(3, 1)
+deplacer(3, 2)
+deplacer(1, 2)
+deplacer(1, 3)
+deplacer(2, 3)
+deplacer(2, 1)
+deplacer(3, 2)
+deplacer(3, 1)
+deplacer(2, 1)
+deplacer(2, 3)
+deplacer(1, 2)
+deplacer(1, 3)
+deplacer(2, 3)
+deplacer(1, 2)
+deplacer(3, 2)
+deplacer(3, 1)
+deplacer(2, 1)
+deplacer(2, 3)
+deplacer(1, 2)
+deplacer(1, 3)
+deplacer(2, 3)
+
+"""
+
+# 6) Recette secrète
+"""
+from robot import *
+
+remplir(5)
+transferer(5, 3)
+vider(3)
+transferer(5, 3)
+remplir(5)
+transferer(5, 3)
+
+"""
+                    # 2 – Répétitions d'instructions.
